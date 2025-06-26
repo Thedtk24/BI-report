3b02b40a76c99d09f9653ce851d17a543f19538e
Thedtk24 <thdarthur@gmail.com>
2025-06-26 22:41:52 +0200

    Script extracteur de données à partir du data store

--- etl/extract.py
+++ etl/extract.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+print("Extraction de données à partir du data store...")
+
 df_calendrier = pd.read_csv("data/raw/calendrier.csv")
 df_employes = pd.read_csv("data/raw/employes.csv")
 df_formations = pd.read_csv("data/raw/formations.csv")
 df_salaires = pd.read_csv("data/raw/salaires.csv")
+
+print("Données enregistrées en dataframes.")
